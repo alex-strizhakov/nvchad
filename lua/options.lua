8e88474d6bbcc38cec9e7f76a05ef3e4a7e383ed
Alexander Strizhakov <alex.strizhakov@gmail.com>
2025-04-18 22:24:24 +0300

    some tweaks

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,35 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
 vim.opt.relativenumber = true
-vim.o.guifont = "FiraCode Nerd Font:h18"
+-- vim.o.guifont = "FiraCode Nerd Font:h18"
+
+-- Restore cursor position
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
+-- Show Nvdash when all buffers are closed
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
